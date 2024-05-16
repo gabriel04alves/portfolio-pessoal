cfcaba2ab1aba91cf6076237db8731e0640f9a5e
Gabriel Alves <gabriel52.alves@gmail.com>
2024-05-15 22:46:35 -0300

    feat: alternar tema

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+const html = document.documentElement
+const btnChangeContext = document.getElementById('change-theme')
+
+function changeContext(context) {
+    html.setAttribute('data-context', context)
+}
+
+btnChangeContext.addEventListener('click', () => {
+    const currentContext = html.getAttribute('data-context')
+    const newContext = currentContext === 'day' ? 'night' : 'day'
+    changeContext(newContext)
+    updateButtonUI(newContext)
+})
+
+function updateButtonUI(context) {
+    const button = document.getElementById('change-theme')
+    if (context === 'day') {
+        button.textContent = '☀️' 
+    } else {
+        button.textContent = '🌙' 
+    }
+}
