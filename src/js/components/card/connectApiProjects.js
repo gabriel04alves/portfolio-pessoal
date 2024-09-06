2484c814f77108be2c62b93be3063afca9c6e98f
Gabriel Alves <gabriel52.alves@gmail.com>
2024-09-06 10:58:25 -0300

    build: substituindo api

--- src/js/components/card/connectApiProjects.js
+++ src/js/components/card/connectApiProjects.js
@@ -1,9 +1,11 @@
 async function getData() {
-    const connect = await fetch('https://api.lspr.dev/api/galves');
-    const connectionJSON = await connect.json()
-    return connectionJSON
+  const connect = await fetch(
+    "https://gabriel04alves.github.io/json-portfolio/db.json"
+  );
+  const connectionJSON = await connect.json();
+  return connectionJSON;
 }
 
 export const connectApi = {
-    getData
-}
+  getData,
+};
