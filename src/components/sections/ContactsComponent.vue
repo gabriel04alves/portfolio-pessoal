4cb253a165275f1541307cf7a9a8a8d7d7071542
Gabriel Alves <gabriel52.alves@gmail.com>
2024-09-28 20:13:42 -0300

    feat: incluindo seção de contatos

---
+++ src/components/sections/ContactsComponent.vue
@@ -0,0 +1,67 @@
+<template>
+    <section id="section-contacts">
+        <h1>
+            <b>
+                Contatos
+            </b>
+        </h1>
+        <div>
+            <a href="https://wa.me/5547984950238">
+                <i class="fab fa-whatsapp"></i>
+                <p>+55 (47) 98495-0238</p>
+            </a>
+            <a href="mailto:gabriel52.alves@gmail.com">
+                <i class="far fa-envelope icon"></i>
+                <p>gabriel52.alves@gmail.com</p>
+            </a>
+            <a href="https://linkedin.com/in/gabriel04alves">
+                <i class="fab fa-linkedin"></i>
+                <p>/gabriel04alves</p>
+            </a>
+            <a href="https://github.com/gabriel04alves">
+                <i class="fab fa-github"></i>
+                <p>/gabriel04alves</p>
+            </a>
+        </div>
+    </section>
+</template>
+
+<script setup></script>
+
+<style scoped>
+section {
+    display: flex;
+    flex-flow: column;
+    padding: 10%;
+    gap: 2vh;
+    background-color: var(--color-background-secondary);
+}
+
+h1 {
+    font-size: 1.8rem;
+    color: var(--color-text-primary);
+}
+
+a {
+    display: flex;
+    align-items: center;
+    gap: 1vw;
+    font-size: 1.3rem;
+    margin: 1.2vh 0 0 0;
+    height: 3vh;
+    width: fit-content;
+    color: var(--color-text-primary);
+    text-decoration: none;
+    transition: transform 0.3s ease;
+}
+
+a:hover {
+    transform: translate(0.5vh, -0.5vh);
+}
+
+@media (min-width: 600px) {
+    section {
+        padding: 5%;
+    }
+}
+</style>
