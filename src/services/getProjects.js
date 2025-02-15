c9d3714daad49275937d514bc136c67cad2cadb4
Gabriel Alves <gabriel52.alves@gmail.com>
2025-02-14 22:03:54 -0300

    feat: update project data source URL in fetchProjects function

--- src/services/getProjects.js
+++ src/services/getProjects.js
@@ -1,7 +1,7 @@
 export async function fetchProjects() {
   try {
     const response = await fetch(
-      "https://raw.githubusercontent.com/gabriel04alves/json-portfolio/refs/heads/dev/db.json?token=GHSAT0AAAAAACWQNOMZGH56KVSYYSEVDRBIZ5JGL6Q"
+      "https://gabriel04alves.github.io/json-portfolio/db.json"
     );
     const data = await response.json();
     return data.projects;
